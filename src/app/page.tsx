1badcb0c57fe4a539d618f62bfcbaac0150f509a
Alison Harris <alisonharris967@gmail.com>
2024-12-09 16:19:10 +0000

    updating geoData function to return location in console

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,56 @@
+'use client'
+
+import { useState } from 'react';
+
+const apiKey = process.env.REACT_APP_API_KEY;
+const baseURL = "http://api.openweathermap.org/geo/1.0/direct";
+const limit = 5;
+
 export default function Home() {
-  return (
-    <>
-        <title>Weather App</title>
-        <form id="weatherForm">
-  <label htmlFor="cityInput">Enter City:</label>
-  <input type="text" id="cityInput" name="city" required />
-  <button type="submit">Get weather</button>
-</form>
-        </>
-  )
-}
+  const [location, setLocation] = useState(''); // State to store the input value
+  const [geoData, setGeoData] = useState({})
 
-const apiKey = process.env.REACT_APP_API_KEY
+  console.log(geoData)
 
-async function getGeoData(location:string) {
-  const baseURL = "http://api.openweathermap.org/geo/1.0/direct";
-  const limit = 5;
-
-  const url = `${baseURL}?q=${encodeURIComponent(location)}&limit=${limit}&appid=${apiKey}`;
-
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`Error: ${response.status} - ${response.statusText}`);
+  async function getGeoData(event: React.MouseEvent<HTMLButtonElement>) {
+    event.preventDefault(); // Prevent the form from submitting and reloading the page
+    if (!location) {
+      console.error('Location is empty');
+      return;
     }
 
-    const data = await response.json();
-    return data
-  } catch (error) {
-    console.error("Failed to fetch geolocation data", error);
-    throw error;
+    const url = `${baseURL}?q=${encodeURIComponent(location)}&limit=${limit}&appid=${apiKey}`;
+
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status} - ${response.statusText}`);
+      }
+
+      const data = await response.json();
+      setGeoData(data);
+
+    } catch (error) {
+      console.error("Failed to fetch geolocation data", error);
+      throw error;
+    }
   }
+
+  return (
+    <>
+      <title>Weather App</title>
+      <form id="weatherForm">
+        <label htmlFor="locationInput">Enter City:</label>
+        <input
+          type="text"
+          id="locationInput"
+          name="location"
+          value={location} // Bind the input value to the state
+          onChange={(e) => setLocation(e.target.value)} // Update state on input change
+          required
+        />
+        <button onClick={getGeoData} type="button">Get weather</button>
+      </form>
+    </>
+  );
 }
-
-getGeoData("London")
-.then(data => console.log(data))
-.catch(error => console.log(error));
-
